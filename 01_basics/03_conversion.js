80fb5a9906028d0dabc8729ef3f006c4de27ccb7
deepansh29 <110782194+deepansh29@users.noreply.github.com>
2024-04-21 18:02:52 +0000

    Conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,14 @@
+let score = "33"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)  //if score="33abc" then output NaN 
+
+let isLoggedIn = 1
+let is=Boolean(isLoggedIn)
+console.log(typeof is)
+console.log(is)
+
+//1=>true   0=>false
+//"deepansh"=>true   ""=>false
