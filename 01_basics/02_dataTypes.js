5511a921fcd0703e18a773ac2fc3ff45ed20fadb
deepansh29 <110782194+deepansh29@users.noreply.github.com>
2024-04-21 17:48:06 +0000

    Variables done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3+3)
+
+//ECMAscript referred or MDN
+
+let name = "Deepansh"
+let age = 18   //Range- 2 to the power 53
+let isLoggedIn = false 
+let state = null
+let un; //undefined
+
+//null=> is also a datatype or standalone value
+//bigint
+//symbol => unique 
+
+//object
+
+console.log(typeof "deepansh")
+console.log(typeof null)
+console.log(typeof undefined)
