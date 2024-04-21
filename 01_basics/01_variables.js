e2e0cc5b6395ac90bf144a87d56c375de62888ac
deepansh29 <110782194+deepansh29@users.noreply.github.com>
2024-04-21 17:47:32 +0000

    Variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail ="deep@google.com"
+var accountPassword = "12345" //not used generally
+// because of the issue in block and functional scope{}
+
+accountCity = "Jaipur" //Don't use
+let accountbal;
+
+// accountId = 2  not allowed
+accountEmail="abc@gmail.com"
+accountPassword = "dfg"
+accountCity = "Alwar"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountbal])
